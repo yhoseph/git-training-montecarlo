e8a867c79ea14037f548ef4b755d53551a43aa29
Ariel <ariel.lenisd@gmail.com>
2019-05-08 01:36:58 -0400

    Add points generator code.

---
+++ MyApplication/MontecarloAlgorithms/MPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MontecarloAlgorithms
+{
+    public struct MPoint
+    {
+        public float X;
+        public float Y;
+
+        public MPoint()
+        {
+            X = Y = 0;
+        }
+
+        public MPoint(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ MyApplication/MontecarloAlgorithms/MonteCarloPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MontecarloAlgorithms
+{
+    public class MonteCarloPI
+    {
+        private List<MPoint> points;
+
+        public MonteCarloPI()
+        {
+            this.points = new List<MPoint>();
+        }
+
+        public void GeneratePoint()
+        {
+            MPoint newPoint = GenerateRandomPoint();
+            this.points.Add(newPoint);
+        }
+
+        private MPoint GenerateRandomPoint()
+        {
+            MPoint newOne = new MPoint(GenerateRandomCoordinate(), GenerateRandomCoordinate());
+            return newOne;
+        }
+
+        private float GenerateRandomCoordinate()
+        {
+            Random rnd = new Random();
+            return (float)rnd.NextDouble();
+        }
+    }
+}
