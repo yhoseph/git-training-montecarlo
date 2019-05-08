ecdc3a0e8a8a205c391340f0865c63a9643d4155
Ariel <ariel.lenisd@gmail.com>
2019-05-08 02:21:21 -0400

    Document montecarlo class before fix random numbers.

--- MyApplication/MontecarloAlgorithms/MonteCarloPI.cs
+++ MyApplication/MontecarloAlgorithms/MonteCarloPI.cs
@@ -1,25 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿/// <summary>
+/// The MonteCarloPI.cs file
+/// </summary>
 namespace MontecarloAlgorithms
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// The MonteCarloPI implementation
+    /// </summary>
     public class MonteCarloPI
     {
         private List<MPoint> points;
         public List<MPoint> Points { get { return points; } }
 
+        /// <summary>
+        /// The MonteCarloPI constructor.
+        /// </summary>
         public MonteCarloPI()
         {
             this.points = new List<MPoint>();
         }
 
+        /// <summary>
+        /// Method to generate a new point.
+        /// </summary>
         public void GeneratePoint()
         {
             MPoint newPoint = GenerateRandomPoint();
             this.points.Add(newPoint);
         }
 
+        /// <summary>
+        /// Method to generate PI.
+        /// </summary>
+        /// <returns></returns>
         public float GetPI()
         {
             int circleCounter = this.points.Count(x=>x.insideCircle);
@@ -28,6 +44,10 @@ namespace MontecarloAlgorithms
             return 4.0f * circleCounter / rectangleCounter;
         }
 
+        /// <summary>
+        /// Generates a new random point.
+        /// </summary>
+        /// <returns>An instance of MPoint.</returns>
         private MPoint GenerateRandomPoint()
         {
             float x = GenerateRandomCoordinate();
@@ -37,6 +57,10 @@ namespace MontecarloAlgorithms
             return newOne;
         }
 
+        /// <summary>
+        /// Generates the random coordinates.
+        /// </summary>
+        /// <returns>The random value.</returns>
         private float GenerateRandomCoordinate()
         {
             Random rnd = new Random();
