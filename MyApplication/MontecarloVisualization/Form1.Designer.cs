01b5020c94203f46340f104d6a31b928aae3c7a7
Ariel <ariel.lenisd@gmail.com>
2019-05-08 01:32:10 -0400

    Draw circle.

--- MyApplication/MontecarloVisualization/Form1.Designer.cs
+++ MyApplication/MontecarloVisualization/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.Color.White;
+            this.picCanvas.Location = new System.Drawing.Point(12, 12);
+            this.picCanvas.Margin = new System.Windows.Forms.Padding(0);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(250, 250);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Click += new System.EventHandler(this.picCanvas_Click);
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.picCanvas_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 551);
+            this.ClientSize = new System.Drawing.Size(271, 270);
+            this.Controls.Add(this.picCanvas);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- MyApplication/MontecarloVisualization/Form1.cs
+++ MyApplication/MontecarloVisualization/Form1.cs
@@ -16,5 +16,33 @@ namespace MontecarloVisualization
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.picCanvas.Invalidate();
+        }
+
+        private void picCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            DrawAll(e.Graphics);
+        }
+
+        private void DrawAll(Graphics graphics)
+        {
+            int w = this.picCanvas.Width;
+            int h = this.picCanvas.Height;
+
+            graphics.DrawEllipse(Pens.Red, 0, 0, w, h);
+        }
+
+        private void picCanvas_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
